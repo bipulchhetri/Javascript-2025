3f33da05036f9e90d5b17eecc9cbfe5960ebcbe8
bipulchhetri <bipulchhetri80@gmail.com>
2025-06-02 11:21:10 +0530

    ticky problem done in map reduce and filter

--- Javascript/NamasteJs/map-filter-reduce.js
+++ Javascript/NamasteJs/map-filter-reduce.js
@@ -33,31 +33,50 @@
 
 
 // Reduce *****
-const arr=[2,3,5,1,7]
+// const arr=[2,3,5,1,7]
 
-function finMax(arr)
-{
-    let max=0
-    for(let i=0;i<arr.length;i++)
-    {
-        if(arr[i]>max)
-        {
-            max = arr[i];
-        }
+// function finMax(arr)
+// {
+//     let max=0
+//     for(let i=0;i<arr.length;i++)
+//     {
+//         if(arr[i]>max)
+//         {
+//             max = arr[i];
+//         }
             
-    }
-    return max;
-}
-// console.log(finMax(arr))
+//     }
+//     return max;
+// }
+// // console.log(finMax(arr))
 
-const output=arr.reduce(function(acc,curr)
+// const output=arr.reduce(function(acc,curr)
+// {
+// if(curr>acc)
+// {
+//     acc=curr;
+// }
+// return acc;
+// },0)
+// console.log(output)
+
+
+// Tricky problem
+
+// const user=[
+//     {fistname:"Bipul",lastName:"hi",age:21},
+//     {fistname:"Raj",lastName:"hffi",age:25},
+//     {fistname:"chhetri",lastName:"ter",age:27},
+//     {fistname:"anything",lastName:"ter",age:27}
+// ]
+// const output1=user.map((x)=>x.age)
+// console.log(output1)
+// const output=user.filter((x)=>x.age<22).map((x)=>x.fistname);
+
+// console.log(output)
+const No=[3,5,2,10];
+const output=No.reduce(function(acc,curr)
 {
-if(curr>acc)
-{
-    acc=curr;
-}
-return acc;
+    return acc+curr;
 },0)
 console.log(output)
-
-
